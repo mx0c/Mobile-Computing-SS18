a149e4807585f902603ef5646273dddfff68c125
Giuseppe Ferrera <giuseppe.ferrera@student.reutlingen-university.de>
2018-05-03 00:41:55 +0200

    added empty dashboard activity, tried to add toolbar as described on googles dev guide. Not working properly yet

---
+++ app/src/main/java/ss18/mc/positime/DashboardActivity.java
@@ -0,0 +1,16 @@
+package ss18.mc.positime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+}
