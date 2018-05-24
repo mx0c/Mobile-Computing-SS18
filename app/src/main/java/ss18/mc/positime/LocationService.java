81f61015c216111d2a25cebaa4ce408c02fa0a11
Marius <marius_matthaeus.bauer@student.reutlingen-university.de>
2018-05-24 14:53:20 +0200

    WIP: LocationService with BroadcastIntent

--- app/src/main/java/ss18/mc/positime/LocationService.java
+++ app/src/main/java/ss18/mc/positime/LocationService.java
@@ -1,163 +1,70 @@
 package ss18.mc.positime;
 
-import android.Manifest;
-import android.app.Activity;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.IBinder;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.widget.Toast;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 
-public class LocationService implements LocationListener {
-    private final Context context;
+public class LocationService extends Service{
 
-    // flag for GPS status
-    boolean isGPSEnabled = false;
+    private LocationListener mListener;
+    private LocationManager mLocationManager;
+    private final static int MIN_DISTANCE_CHANGE_FOR_UPDATES = 0;
+    private final static int MIN_TIME_BW_UPDATES = 10000;
 
-    // flag for network status
-    boolean isNetworkEnabled = false;
-
-    // flag for GPS status
-    boolean canGetLocation = false;
-
-    Location location; // location
-    double latitude; // latitude
-    double longitude; // longitude
-
-    // The minimum distance to change Updates in METERS
-    private static int MIN_DISTANCE_CHANGE_FOR_UPDATES;
-
-    // The minimum time between updates in MILLISECONDS
-    private static int MIN_TIME_BW_UPDATES;
-
-    //GPS permission code
-    private final static int REQUEST_GPS_PERMISSION = 2;
-
-    // Declaring a Location Manager
-    protected LocationManager locationManager;
-
-    public LocationService(){
-        context = null;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        //not binded to specific Activity
+        return null;
     }
 
-    public LocationService(Context context, int updatetime, int updatedistance) {
-        this.context = context;
-        this.MIN_DISTANCE_CHANGE_FOR_UPDATES = updatedistance;
-        this.MIN_TIME_BW_UPDATES = updatetime;
-        getLocation();
-    }
-
-    public Location getLocation() {
-        try {
-            locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-
-            // getting GPS status
-            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-            // getting network status
-            isNetworkEnabled = locationManager
-                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-            if (!isGPSEnabled && !isNetworkEnabled) {
-                // no network provider is enabled
-            } else {
-                this.canGetLocation = true;
-                // First get location from Network Provider
-                if (isNetworkEnabled) {
-                    if(ActivityCompat.checkSelfPermission(this.context, Manifest.permission.ACCESS_FINE_LOCATION)
-                            != PackageManager.PERMISSION_GRANTED) {
-                        locationManager.requestLocationUpdates(
-                                LocationManager.NETWORK_PROVIDER,
-                                MIN_TIME_BW_UPDATES,
-                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                    }else{
-                        ActivityCompat.requestPermissions((Activity)context, new String[]{ Manifest.permission.ACCESS_FINE_LOCATION },
-                                REQUEST_GPS_PERMISSION);
-                    }
-                    if (locationManager != null) {
-                        location = locationManager
-                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                    }
-                }
-
-                // if GPS Enabled get lat/long using GPS Services
-                if (isGPSEnabled) {
-                    if (location == null) {
-                        if(ActivityCompat.checkSelfPermission(this.context, Manifest.permission.ACCESS_FINE_LOCATION)
-                                != PackageManager.PERMISSION_GRANTED) {
-                            locationManager.requestLocationUpdates(
-                                    LocationManager.GPS_PROVIDER,
-                                    MIN_TIME_BW_UPDATES,
-                                    MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                        }else{
-                            ActivityCompat.requestPermissions((Activity)context, new String[]{ Manifest.permission.ACCESS_FINE_LOCATION },
-                                    REQUEST_GPS_PERMISSION);
-                        }
-                        if (locationManager != null) {
-                            location = locationManager
-                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        }
-                    }
-                }
+    @Override
+    @SuppressWarnings({"MissingPermission"})
+    public void onCreate() {
+        mListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Intent i = new Intent("location_update");
+                i.putExtra("longitude",location.getLongitude());
+                i.putExtra("latitude",location.getLatitude());
+                sendBroadcast(i);
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
 
-        return location;
-    }
+            }
 
-    // Calling this function will stop the GPS listener
-    public void stopUsingGPS() {
-        if (locationManager != null) {
-            locationManager.removeUpdates(this);
-        }
-    }
+            @Override
+            public void onProviderEnabled(String s) {
 
-    public double getLatitude() {
-        if (location != null) {
-            latitude = location.getLatitude();
-        }
+            }
 
-        // return latitude
-        return latitude;
-    }
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        };
 
-    public double getLongitude() {
-        if (location != null) {
-            longitude = location.getLongitude();
-        }
-
-        // return longitude
-        return longitude;
-    }
-
-    //returns if GPS or wifi is enabled
-    public boolean canGetLocation() {
-        return this.canGetLocation;
+        mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,MIN_TIME_BW_UPDATES,MIN_DISTANCE_CHANGE_FOR_UPDATES,mListener);
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-        Toast.makeText(context, "LocationChanged", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
+    @SuppressWarnings({"MissingPermission"})
+    public void onDestroy() {
+        super.onDestroy();
+        if(mLocationManager != null){
+            mLocationManager.removeUpdates(mListener);
+        }
     }
 }
--- app/src/main/java/ss18/mc/positime/MainActivity.java
+++ app/src/main/java/ss18/mc/positime/MainActivity.java
@@ -1,16 +1,74 @@
 package ss18.mc.positime;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver broadcastReceiver;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runtime_permissions();
+        tv = (TextView) findViewById(R.id.textView);
+        Intent i =new Intent(getApplicationContext(),LocationService.class);
+        startService(i);
+    }
 
-        LocationChecker lc = new LocationChecker(this,0,0);
-        lc.setAlarm(this);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(broadcastReceiver == null){
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    tv.append("\n" +intent.getExtras().get("latitude")+ " : " + intent.getExtras().get("longitude"));
+                }
+            };
+        }
+        registerReceiver(broadcastReceiver,new IntentFilter("location_update"));
+    }
+
+    private boolean runtime_permissions() {
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},100);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Intent i =new Intent(getApplicationContext(),LocationService.class);
+        stopService(i);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100){
+            if( grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                return;
+            }else {
+                runtime_permissions();
+            }
+        }
     }
 }
