a1ec5396238f72657d7ac44fd3e7d51fa5bbefbe
Marius <marius_matthaeus.bauer@student.reutlingen-university.de>
2018-05-25 11:50:56 +0200

    BackgroundService & Dialog when GPS disabled

---
+++ app/src/main/java/ss18/mc/positime/BackgroundService.java
@@ -0,0 +1,42 @@
+package ss18.mc.positime;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+
+public class BackgroundService extends Service {
+    private BroadcastReceiver broadcastReceiver;
+
+    @Override
+    public void onCreate() {
+        //start LocationService
+        Intent i =new Intent(getApplicationContext(),LocationService.class);
+        startService(i);
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                //intent.getExtras().get("latitude")
+                //intent.getExtras().get("longitude")
+            }
+        };
+    registerReceiver(broadcastReceiver,new IntentFilter("location_update"));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //stop LocationService
+        Intent i =new Intent(getApplicationContext(),LocationService.class);
+        stopService(i);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        //not binded to specific Activity
+        return null;
+    }
+}
--- app/src/main/java/ss18/mc/positime/LocationService.java
+++ app/src/main/java/ss18/mc/positime/LocationService.java
@@ -1,7 +1,9 @@
 package ss18.mc.positime;
 
+import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
@@ -10,12 +12,13 @@ import android.os.IBinder;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.view.WindowManager;
 
 public class LocationService extends Service{
 
     private LocationListener mListener;
     private LocationManager mLocationManager;
-    private final static int MIN_DISTANCE_CHANGE_FOR_UPDATES = 0;
+    private final static int MIN_DISTANCE_CHANGE_FOR_UPDATES = 5;
     private final static int MIN_TIME_BW_UPDATES = 10000;
 
     @Nullable
@@ -49,9 +52,26 @@ public class LocationService extends Service{
 
             @Override
             public void onProviderDisabled(String s) {
-                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(i);
+                final AlertDialog alertDialog = new AlertDialog.Builder(getApplicationContext()).create();
+                alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+                alertDialog.setTitle("GPS is Disabled");
+                alertDialog.setMessage("Do you want to enable GPS?");
+                alertDialog.setButton("OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(i);
+                            dialog.dismiss();
+                        }
+                });
+                alertDialog.setButton2("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        return;
+                    }
+                });
+                alertDialog.show();
             }
         };
 
