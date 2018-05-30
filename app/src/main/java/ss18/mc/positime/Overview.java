f83fefdf7d44c70458668c8bb24e9b5074f9878b
lessig <essiglena@gmail.com>
2018-05-30 14:13:33 +0200

    added sidebar

--- app/src/main/java/ss18/mc/positime/Overview.java
+++ app/src/main/java/ss18/mc/positime/Overview.java
@@ -1,18 +1,42 @@
 package ss18.mc.positime;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class Overview extends AppCompatActivity {
+import ss18.mc.positime.utils.Constants;
+
+public class Overview extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Workplaces");
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         button= findViewById(R.id.workPlace1);
         button.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +46,56 @@ public class Overview extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+
+    //When logout is clicked, remove token and go back to login
+    public void onLogoutClick(MenuItem view) {
+        switch(view.getItemId()){
+            case R.id.logout_icon:
+
+                //Reset token
+                SharedPreferences mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+                SharedPreferences.Editor editor = mSharedPreferences.edit();
+                editor.putString(Constants.TOKEN,null);
+                editor.putString(Constants.EMAIL,null);
+                editor.apply();
+
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_activity, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_dashboard) {
+            // Handle the camera action
+        } else if (id == R.id.nav_overview) {
+
+        } else if (id == R.id.nav_workplaces) {
+
+        } else if (id == R.id.nav_statistics) {
+
+        } else if (id == R.id.nav_export) {
+
+        } else if (id == R.id.nav_import) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
