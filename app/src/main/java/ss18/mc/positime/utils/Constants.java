43a2dc4340fc1fc0d8ea8517fbd321e723ae91dc
Giuseppe Ferrera <giuseppe.ferrera@student.reutlingen-university.de>
2018-06-06 21:31:06 +0200

    fixed that app crashed in older version of android because of drawables

--- app/src/main/java/ss18/mc/positime/utils/Constants.java
+++ app/src/main/java/ss18/mc/positime/utils/Constants.java
@@ -1,8 +1,9 @@
 package ss18.mc.positime.utils;
 
 public class Constants {
-    //HS SErver IP 134.103.176.164
-    public static final String BASE_URL = "http://134.103.176.164:8080/api/v1/"; //CHANGE THE IP ADDRESS
+    //HS Server IP 134.103.176.164
+    //Giuseppe PC IP 192.168.0.129
+    public static final String BASE_URL = "http://192.168.0.129:8080/api/v1/"; //CHANGE THE IP ADDRESS
     public static final String TOKEN = "token";
     public static final String EMAIL = "email";
     public static final String PASSWORD = "hashed_password";
