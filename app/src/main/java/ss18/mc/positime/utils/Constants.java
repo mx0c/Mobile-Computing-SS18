c469fce61b462503b1b71c3931997c30b26b4bda
lessig <essiglena@gmail.com>
2018-06-06 08:51:49 +0200

    new branch for workplace details

--- app/src/main/java/ss18/mc/positime/utils/Constants.java
+++ app/src/main/java/ss18/mc/positime/utils/Constants.java
@@ -1,7 +1,7 @@
 package ss18.mc.positime.utils;
 
 public class Constants {
-    public static final String BASE_URL = "http://192.168.0.129:8080/api/v1/"; //CHANGE THE IP ADDRESS
+    public static final String BASE_URL = "http://192.168.178.47:8080/api/v1/"; //CHANGE THE IP ADDRESS
     public static final String TOKEN = "token";
     public static final String EMAIL = "email";
     public static final String PASSWORD = "hashed_password";
