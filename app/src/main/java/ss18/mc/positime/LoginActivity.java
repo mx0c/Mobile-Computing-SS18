17db66b16a182a6bea03bce3a02b24e0e285a834
mx0c <marius-bauer-2006@web.de>
2018-06-10 16:59:37 +0200

    fix

--- app/src/main/java/ss18/mc/positime/LoginActivity.java
+++ app/src/main/java/ss18/mc/positime/LoginActivity.java
@@ -279,6 +279,8 @@ public class LoginActivity extends AppCompatActivity {
             check_gps();
             Intent i = new Intent(this, LocationService.class);
             startService(i);
+            i = new Intent(this, BackgroundService.class);
+            startService(i);
         }else {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
