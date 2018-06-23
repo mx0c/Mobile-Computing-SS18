b3085d4dda62e95037f540bf031ce63b706a29b9
Marius <marius_matthaeus.bauer@student.reutlingen-university.de>
2018-06-23 12:28:43 +0200

    fixed profile navbar

--- app/src/main/java/ss18/mc/positime/ProfileActivity.java
+++ app/src/main/java/ss18/mc/positime/ProfileActivity.java
@@ -122,10 +122,30 @@ public class ProfileActivity extends AppCompatActivity implements OnNavigationIt
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.navigation_activity, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
